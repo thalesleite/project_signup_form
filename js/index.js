1ef22cd45a6096f3faaf7f06d1ac5949380cd370
thalesleite <thalesaleite@gmail.com>
2024-04-06 17:56:03 +0100

    add button create account and password simple validation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log("I'm running!!!")
+const password = document.getElementById("password")
+const confirmPassword = document.getElementById("confirm_password")
+const loginForm = document.getElementById("loginForm")
+
+const error = document.getElementById("error")
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  error.innerText = ""
+
+  if (password.value !== confirmPassword.value) {
+    error.innerText = "* Passwords do not match"
+  }
+})
